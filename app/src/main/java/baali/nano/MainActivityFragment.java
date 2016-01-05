b4b45cc073becf16ad9b5b20549de2ea96beac8f
balaji-manogar-cs <balaji.manogar.cs@gmail.com>
2016-01-05 08:12:34 +0530

    Added key in gradle

--- app/src/main/java/baali/nano/MainActivityFragment.java
+++ app/src/main/java/baali/nano/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package baali.nano;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,17 +9,14 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment
-{
+public class MainActivityFragment extends Fragment {
 
-    public MainActivityFragment()
-    {
+    public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
+            Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
